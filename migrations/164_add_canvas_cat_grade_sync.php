d8960ff09a2734d5090b63418cb3badcc15534dd
Ondrej Zjevik <zjevi001@d.umn.edu>
2019-05-27 11:46:46 -0400

    Description correction

--- migrations/164_add_canvas_cat_grade_sync.php
+++ migrations/164_add_canvas_cat_grade_sync.php
@@ -54,6 +54,7 @@ $query = "CREATE TABLE `imas_lti_gbcatqueue` (
   
 $DBH->commit();
 
-echo "<p style='color: green;'>✓ Added columns to imas_assessments for Live Poll location</p>";
+echo "<p style='color: green;'>✓ Added column to imas_assessments for Canvas gradesync</p>";
+echo "<p style='color: green;'>✓ Added tables imas_lti_gbcat, imas_lti_gbcatqueue</p>";
 
 return true;
